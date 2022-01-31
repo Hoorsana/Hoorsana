77af211e3d9540397cacac73741c1fe5b92b22f7
Hoorsana <hoorsanaamjadi@gmail.com>
2022-01-31 13:26:43 +0100

    'For_Schleife'

--- test_KugelaufPlatte.py
+++ test_KugelaufPlatte.py
@@ -4,6 +4,7 @@ import pytest
 import scipy
 import matplotlib.pylab as plt
 from scipy.interpolate import InterpolatedUnivariateSpline
+import scipy.integrate
 from pylab.core import timeseries
 import numpy
 import numpy as np
@@ -12,7 +13,7 @@ from scipy import integrate
 from pylab.core import loader
 from pylab.core import testing
 from pylab.simulink import simulink
-
+from collections import coun
 from pylab.simulink import _engine
 
 
@@ -22,26 +23,182 @@ _engine.import_matlab_engine("R2021b")
 def test_experiment():
     info = loader.load_test("test.yml")
     details = simulink.load_details("matlab_detail.yml")
-    # details.devices[0].data["params"]["Kr"] = 0.6
-    # details.devices[0].data["params"]["D"][1] = 0.2
-    # print(details.devices[0].data["params"])
-    experiment = simulink.create(info, details)
-    report = experiment.execute()
-    if report.failed:
-        raise AssertionError(report.what)
-    result = report.results["PT1Regler.Alpha"]
-    s = result.pretty_string()
-    a = result.values
-    asign = np.sign(a)
-    signchange = ((np.roll(asign, 1) - asign) != 0).astype(int)
-    A = signchange
-    G = np.where(A == 1)
-    print(G)
-    T = result.time
-    print(T[G[0]])
-    finterp = InterpolatedUnivariateSpline(T, a, k=1)
-    qq = [finterp.integral(0, t) for t in T
-    print(qq)
+    details.devices[0].data["params"]["D"][1] = 0.2
+    Kr = details.devices[0].data["params"]["Kr"]
+    I = [r * 0.01 for r in range(0, 50, 5)]
+    Kr_List = []
+    Ueberschwing_Alpha = []
+    Ueberschwing_X = []
+    Schwing_Alpha = []
+    Schwing_X = []
+    E_Alpha = []
+    E_X = []
+    Z1 = []
+    Z2 = []
+    for i in I:
+       details.devices[0].data["params"]["Kr"] = 0.6+i
+       experiment = simulink.create(info, details)
+       report = experiment.execute()
+       if report.failed:
+           raise AssertionError(report.what)
+       print("Kr:", float(0.45+i))
+       Kr_List.append(float(0.45+i))
+                                       #TODO Alpha
+       result = report.results["PT1Regler.Alpha"]
+       alpha = result.pretty_string()
+       alpha_V = result.values
+       alpha_T = result.time
+                                       #TODO Schwingweite Alpha
+       Ueberschwing1 = max(alpha_V)
+       Schwingweite1 = max(result.values)+abs(min(result.values))
+       Ueberschwing_Alpha.append(Ueberschwing1)
+       Schwing_Alpha.append(Schwingweite1)
+                                       #TODO Energie von Alpha
+       f_Alpha = InterpolatedUnivariateSpline(alpha_T, alpha_V, k=1)
+       Energie_Alpha = scipy.integrate.quad_vec(lambda t: np.absolute(f_Alpha(t)), 0, alpha_T[-1])
+       E_Alpha.append(Energie_Alpha)
+                                      #TODO Zero Crossing
+
+       z1 = timeseries.zero_crossings(result, 1e-9)
+       Z1.append(z1)
+
+                                       #TODO X_Ist
+       result1 = report.results["PT1Regler.x_Ist"]
+       x_Ist = result1.pretty_string()
+       x_V = result1.values
+       x_T = result1.time
+                                    # TODO Schwingweite X_Ist
+       Ueberschwing2 = max(result1.values)
+       Schwingweite2 = max(result1.values) + abs(min(result1.values))
+       Ueberschwing_X.append(Ueberschwing2)
+       Schwing_X.append(Schwingweite2)
+                                       #TODO Energie von X_Ist
+       f_X = InterpolatedUnivariateSpline(x_T, x_V, k=1)
+       Energie_X = scipy.integrate.quad_vec(lambda t: np.absolute(f_X(t)), 0, x_T[-1])
+       E_X.append(Energie_X)
+                                        # TODO Zero Crossing
+
+       z2 = timeseries.zero_crossings(result1, 1e-9)
+       Z2.append(z2)
+
+    print(Kr_List)
+    print("Zero Crossings:",Z)
+    print("Uberschwingweite von Alpha:", Ueberschwing_Alpha)
+    print("Uberschwingweite von x_Ist:", Ueberschwing_X)
+    print("Schwingweite von Alpha:", Schwing_Alpha)
+    print("Schwingweite von x_Ist:", Schwing_X)
+    print("Energei von Alpha:", E_Alpha)
+    print("Energei von x_Ist:", E_X)
+    K1 = Kr_List[Ueberschwing_Alpha.index(min(Ueberschwing_Alpha))]
+    K2 = Kr_List[Ueberschwing_X.index(min(Ueberschwing_X))]
+    K3 = Kr_List[E_Alpha.index(min(E_Alpha))]
+    K4 = Kr_List[E_X.index(min(E_X))]
+    K5 = Kr_List[Z1.index(min(Z1))]
+    K6 = Kr_List[Z2.index(min(Z2))]
+    Ki_List = [float(K1), float(K2), float(K3), float(K4), float(K5), float(6)]
+    print(Ki_List)
+    d = defaultdict(float)
+    for i in Ki_List:
+        d[i] += 1
+    most_frequent = sorted(Counter(L).most_common(), key=lambda x: x[1], reverse=True)[0]
+
+
+
+
+
+
+    # counter = 0
+    # num = Ki_List[0]
+    # for i in Ki_List:
+    #     curr_frequncy = Ki_List.count(i)
+    #     if (curr_frequency > counter):
+    #         counter = curr_frequncy
+    #         num = i
+    # return num
+
+
+
+
+    # def test_experiment():
+    #     info = loader.load_test("test.yml")
+    #     details = simulink.load_details("matlab_detail.yml")
+    #     details.devices[0].data["params"]["D"][1] = 0.2
+    #     Kr = details.devices[0].data["params"]["Kr"]
+    #     I = [r * 0.001 for r in range(0, 30, 5)]
+    #     Kr_List = []
+    #     Ueberschwing_Alpha = []
+    #     Ueberschwing_X = []
+    #     Schwing_Alpha = []
+    #     Schwing_X = []
+    #     E_Alpha = []
+    #     E_X = []
+    #     Z1 = []
+    #     Z2 = []
+    #     for i in I:
+    #         details.devices[0].data["params"]["D"][1] = 0.2+i
+    #         experiment = simulink.create(info, details)
+    #         report = experiment.execute()
+    #         if report.failed:
+    #             raise AssertionError(report.what)
+    #         print("Td:", float(0.45 + i))
+    #         Kr_List.append(float(0.45 + i))
+    #         # TODO Alpha
+    #         result = report.results["PT1Regler.Alpha"]
+    #         alpha = result.pretty_string()
+    #         alpha_V = result.values
+    #         alpha_T = result.time
+    #         # TODO Schwingweite Alpha
+    #         Ueberschwing1 = max(alpha_V)
+    #         Schwingweite1 = max(result.values) + abs(min(result.values))
+    #         Ueberschwing_Alpha.append(Ueberschwing1)
+    #         Schwing_Alpha.append(Schwingweite1)
+    #         # TODO Energie von Alpha
+    #         f_Alpha = InterpolatedUnivariateSpline(alpha_T, alpha_V, k=1)
+    #         Energie_Alpha = scipy.integrate.quad_vec(lambda t: np.absolute(f_Alpha(t)), 0, alpha_T[-1])
+    #         E_Alpha.append(Energie_Alpha)
+    #         # TODO Zero Crossing
+    #
+    #         z1 = timeseries.zero_crossings(result, 1e-9)
+    #         Z1.append(z1)
+    #
+    #         # TODO X_Ist
+    #         result1 = report.results["PT1Regler.x_Ist"]
+    #         x_Ist = result1.pretty_string()
+    #         x_V = result1.values
+    #         x_T = result1.time
+    #         # TODO Schwingweite X_Ist
+    #         Ueberschwing2 = (resulmaxt1.values)
+    #         Schwingweite2 = max(result1.values) + abs(min(result1.values))
+    #         Ueberschwing_X.append(Ueberschwing2)
+    #         Schwing_X.append(Schwingweite2)
+    #         # TODO Energie von X_Ist
+    #         f_X = InterpolatedUnivariateSpline(x_T, x_V, k=1)
+    #         Energie_X = scipy.integrate.quad_vec(lambda t: np.absolute(f_X(t)), 0, x_T[-1])
+    #         E_X.append(Energie_X)
+    #         # TODO Zero Crossing
+    #
+    #         z2 = timeseries.zero_crossings(result1, 1e-9)
+    #         Z2.append(z2)
+
+       # T = result.time
+       # print(T[G[0]])
+       # f = InterpolatedUnivariateSpline(T, a, k=1)
+       # f_int = scipy.integrate.quad_vec(lambda t: np.absolute(f(t)), 0, T[-1])
+       # print(f_int)
+       # B = []
+       # for i in a:
+       #     if abs(i)<1e-9:
+       #         continue
+       #         B.append(i)
+       # return B
+       # print(B)
+    # y = f(T)
+    # print(y)
+    # abs_Y = abs(f(T))
+    # print(abs_Y)
+    # y_int = integrate.cumtrapz(abs_Y, T, initial=0)
+    # qq = [Y.integral(0, t) for t in T]
+    # print(qq)
     # print(s)
     # A = result.values
     # print(A)
@@ -131,3 +288,9 @@ def test_experiment():
     #     10,
     #     atol = 0.01
     # )
+    # a = result.values
+    # asign = np.sign(a)
+    # signchange = ((np.roll(asign, 1) - asign) != 0).astype(int)
+    # A = signchange
+    # G = [np.where(A == 1)]
+    # G.append(G)
